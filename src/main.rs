ab35d8a589e3e17bfc1a8ab7d070021453575752
d-kja <89911360+d-kja@users.noreply.github.com>
2025-05-30 19:35:21 -0300

    awda

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use core::entity::VENDOR_ID;
+use core::entity::{PRODUCT_ID, VENDOR_ID};
 use std::error::Error;
 
 use mirajazz::device::{list_devices, new_hidapi};
+use tracing::info;
 
 mod core;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Starting up...");
+    tracing_subscriber::fmt::init();
+
+    info!("Setting up and reading devices.");
 
     let hidapi = new_hidapi()?;
     let devices = list_devices(&hidapi, &[VENDOR_ID]);
 
+    info!("Found {:?} connected devices", devices.len());
+
     for (vendor_id, product_id, serial) in devices {
-        println!("[INFO] - Item found:\n - Vendor: {}\n - Product: {}\n - Serial: {}", vendor_id, product_id, serial);
+        if product_id != PRODUCT_ID {
+            continue;
+        }
+
+        info!("Deck is connected using {}:{}", vendor_id, product_id);
     }
 
     Ok(())
