97acfb77d37437fe3260205c749ba462f50e689b
d-kja <89911360+d-kja@users.noreply.github.com>
2025-05-30 19:55:19 -0300

    awda

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,65 @@
-use core::entity::{PRODUCT_ID, VENDOR_ID};
-use std::error::Error;
-
-use mirajazz::device::{list_devices, new_hidapi};
-use tracing::info;
-
 mod core;
+
+use core::entity::{KEY_COUNT, PRODUCT_ID, VENDOR_ID};
+use mirajazz::device::{Device, list_devices, new_hidapi};
+
+use owo_colors::OwoColorize;
+use std::{error::Error, process::exit};
+use tracing::{error, info, warn};
 
 fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
 
-    info!("Setting up and reading devices.");
+    info!("Setting up hidapi to connect with device.");
+    let hidapi = match new_hidapi() {
+        Ok(value) => value,
+        Err(err) => panic!("Unable to create hidapi instance, reason: {:?}", err),
+    };
 
-    let hidapi = new_hidapi()?;
     let devices = list_devices(&hidapi, &[VENDOR_ID]);
+    info!("Found a total of {:?} connected devices", devices.len());
 
-    info!("Found {:?} connected devices", devices.len());
-
+    let mut hid_device: Option<(u16, u16, String)> = None;
     for (vendor_id, product_id, serial) in devices {
         if product_id != PRODUCT_ID {
             continue;
         }
 
-        info!("Deck is connected using {}:{}", vendor_id, product_id);
+        info!(
+            "Deck is connected using {:04X}:{:04X}",
+            vendor_id, product_id
+        );
+        hid_device = Some((vendor_id, product_id, serial));
+        break;
     }
+
+    if hid_device.is_none() {
+        panic!("Device not found!~");
+    }
+
+    let (vendor_id, product_id, serial) = hid_device.unwrap();
+    let device = match Device::connect(
+        &hidapi,
+        vendor_id,
+        product_id,
+        &serial,
+        false,
+        false,
+        KEY_COUNT as usize,
+        0,
+    ) {
+        Ok(value) => value,
+        Err(err) => panic!("Unable to connect to device, reason: {:?}", err),
+    };
+
+    let count = device.key_count();
+    info!("Device has a total of {} keys", count);
+
+    info!("Updating brightness to 100%");
+    device.set_brightness(100)?;
+
+    info!("Resetting all the buttons");
+    device.clear_all_button_images()?;
 
     Ok(())
 }
